f2edc3cc4a5952cf018c117ac7d1da182b34c5ff
unknown <edavid.rd@gmail.com>
2024-04-21 08:19:07 -0400

    Menu mobile

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import './App.css'
 import logo from './logo.svg'
 import basket from './assets/icons/basket.svg'
+import menuOpen from './assets/icons/burger.svg'
+import menuClose from './assets/icons/Multiply.svg'
 
 // React Components
 import { Routes, Route, Link } from 'react-router-dom'
@@ -18,11 +20,13 @@ import Signin from './components/Signin'
 import Signup from './components/Signup'
 
 // Functions
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
+// Variables
+var menuIcon = menuOpen
 
 function App() {
-  var [state, setState] = useState('false')
+  var [state, setState] = useState('100')
   const handleClick = () => () => {
     const element = document.getElementById('about');
     if (element) {
@@ -35,34 +39,66 @@ function App() {
 
   const handleMenu = () =>
   {
-    if (state === "false") {
-      setState('True')
-    } else if (state === "True") {
-      setState("false")
+    if (state === "100") {
+      setState('0')
+      menuIcon = menuClose
+    } else if (state === "0") {
+      setState("100")
+      menuIcon = menuOpen
     }
-    console.log(state)
   }
 
-  
-
-  useEffect(() => {
-    
-  }, [])
+  // element styles
+  const navStyle = 
+  {
+    position: "fixed",
+    inset: "0 0 0 16%",
+    margin: 0,
+    display: "flex",
+    flexDirection: "column",
+    zIndex: 1000,
+    transform: `translateX(${state}%)`,
+    transition: "transform 350ms ease-out",
+    backgroundColor: "hsl(0 0% 100% / 0.1)",
+    backdropFilter: "blur(2rem)",
+  }
+  const buttonStyle = 
+  {
+    display: "block",
+    zIndex: 9999,
+    position: "absolute",
+    background: "none",
+    backgroundRepeat: "no-repeat",
+    width:"5svh",
+    aspectRatio: 1,
+    top: "2.5svh",
+    right: "4vw",
+    borderRadius: 0,
+  }
 
   return (
     <>
       <div className='navback'>
-        <Link to='/littlelemon' className='logo'><img className='logo' src={logo} alt="logo"/></Link>
         
-        <button className='mobile-nav-toggle'
-        aria-controls='primary-nav' aria-expanded="false"
-        onClick={handleMenu}></button>
+        <img className='basket-icon' id='mobile' src={basket} alt='shop'/>
 
-        <nav id='primary-nav' className='navbar align' 
-          data-visible="false">
+        <Link to='/littlelemon' className='logo'><img className='logo' src={logo} alt="logo"/></Link>
 
-          <img className='basket-icon' id='mobile' src={basket} alt='shop'/>
+        <button 
+          className='mobile-nav-toggle'
+          aria-controls='primary-nav' 
+          aria-expanded="false"
+          onClick={handleMenu}
+          style={buttonStyle}
+        ><img src={menuIcon} alt='menu' width='100%'/></button>
+
+        <nav
+          id='primary-nav' 
+          className='navbar align' 
+          style={navStyle}
+        >
           <div className='links'>
+          <Link to='/littlelemon' className='logo'><img className='logo' src={logo} alt="logo"/></Link>
             <Link to='/littlelemon' className='link'>Home</Link>
             <a href='/littlelemon/#about' className='link' onClick={handleClick}>About</a>
             <Link to='/littlelemon/menu' className='link'>Menu</Link>
@@ -70,6 +106,7 @@ function App() {
             <Link to='/littlelemon/order' className='link'>Order Online</Link>
             <Link to='/littlelemon/login' className='link'>Login</Link>
           </div>
+
         </nav>
       </div>
       <main className='content'>
