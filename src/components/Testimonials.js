b7f4f876377300c05baabce1cb202e46bbe3d4a9
thedatadj <edavid.rd@gmail.com>
2024-06-07 14:06:21 +0100

    Testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,14 +1,32 @@
 import '../styles/testimonials.css'
 
+
+
 const Testimonials = () => {
     return (
         <div className='testimonials-area'>
             <div className="testimonials align">
                 <h1>Testimonials</h1>
-                <div className="review box1"></div>
-                <div className="review box2"></div>
-                <div className="review box3"></div>
-                <div className="review box4"></div>
+                <div className="review box1">
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                </div>
+                <div className="review box2">
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                </div>
+                <div className="review box3">
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                </div>
+                <div className="review box4">
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                </div>
             </div>
         </div>
     )
